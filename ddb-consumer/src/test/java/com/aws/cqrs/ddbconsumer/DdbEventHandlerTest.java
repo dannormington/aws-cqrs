8c33e14f6cbd230d685c733b4d49d0efbb5ebf81
dannormington <dnorm252@gmail.com>
2024-10-18 19:18:10 -0500

    Added unit tests for DdbEventHandler

--- ddb-consumer/src/test/java/com/aws/cqrs/ddbconsumer/DdbEventHandlerTest.java
+++ ddb-consumer/src/test/java/com/aws/cqrs/ddbconsumer/DdbEventHandlerTest.java
@@ -1,14 +1,24 @@
 package com.aws.cqrs.ddbconsumer;
 
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.StreamRecord;
+import com.aws.cqrs.ddbconsumer.exceptions.DeserializationException;
+import com.aws.cqrs.domain.AccountCreated;
+import com.aws.cqrs.infrastructure.messaging.Event;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 class DdbEventHandlerTest {
+
+    private final Gson gson = new Gson();
 
     @Test
     void when_handleRequest_no_events_expect_success() {
@@ -20,6 +30,66 @@ class DdbEventHandlerTest {
 
         // Act
         ddbEventHandler.handleRequest(dynamodbEvent, null);
+
+        // Assert
+        verifyNoInteractions(eventBus);
+    }
+
+    @Test
+    void when_handleRequest_expect_success() {
+        // Arrange
+        EventBus eventBus = mock(EventBus.class);
+        DdbEventHandler ddbEventHandler = new DdbEventHandler(eventBus);
+        DynamodbEvent dynamodbEvent = new DynamodbEvent();
+
+        Map<String, AttributeValue> attributes = new HashMap<>();
+        AccountCreated accountCreated = new AccountCreated(UUID.randomUUID(), "John", "Smith");
+        attributes.put("Kind", new AttributeValue().withS(accountCreated.getClass().getName()));
+        attributes.put("Event", new AttributeValue().withS(gson.toJson(accountCreated)));
+
+        List<DynamodbEvent.DynamodbStreamRecord> records = new ArrayList<>();
+        DynamodbEvent.DynamodbStreamRecord dynamodbStreamRecord = new DynamodbEvent.DynamodbStreamRecord();
+        dynamodbStreamRecord.setDynamodb(new StreamRecord().withNewImage(attributes));
+        dynamodbStreamRecord.getDynamodb().setKeys(Map.of("Id", new AttributeValue().withS(UUID.randomUUID().toString())));
+        records.add(dynamodbStreamRecord);
+        dynamodbEvent.setRecords(records);
+
+        doReturn(CompletableFuture.completedFuture(true)).when(eventBus).handle(any(Event.class));
+
+        // Act
+        ddbEventHandler.handleRequest(dynamodbEvent, null);
+
+        // Assert
+        verify(eventBus, times(1)).handle(any(Event.class));
+    }
+
+    @Test
+    void when_handleRequest_expect_DeserializationException() {
+        // Arrange
+        EventBus eventBus = mock(EventBus.class);
+        DdbEventHandler ddbEventHandler = new DdbEventHandler(eventBus);
+        DynamodbEvent dynamodbEvent = new DynamodbEvent();
+
+        Map<String, AttributeValue> attributes = new HashMap<>();
+        AccountCreated accountCreated = new AccountCreated(UUID.randomUUID(), "John", "Smith");
+        attributes.put("Kind", new AttributeValue().withS("invalid-class-name"));
+        attributes.put("Event", new AttributeValue().withS(gson.toJson(accountCreated)));
+
+        List<DynamodbEvent.DynamodbStreamRecord> records = new ArrayList<>();
+        DynamodbEvent.DynamodbStreamRecord dynamodbStreamRecord = new DynamodbEvent.DynamodbStreamRecord();
+        dynamodbStreamRecord.setDynamodb(new StreamRecord().withNewImage(attributes));
+        dynamodbStreamRecord.getDynamodb().setKeys(Map.of("Id", new AttributeValue().withS(UUID.randomUUID().toString())));
+        records.add(dynamodbStreamRecord);
+        dynamodbEvent.setRecords(records);
+
+        // Act & Assert
+        assertThrows(DeserializationException.class, () -> {
+            try {
+                ddbEventHandler.handleRequest(dynamodbEvent, null);
+            } catch (CompletionException x) {
+                throw x.getCause();
+            }
+        });
 
         // Assert
         verifyNoInteractions(eventBus);
